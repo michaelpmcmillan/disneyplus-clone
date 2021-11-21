7111329384101a2b45680dc75ae37de698859ff7
Mike McMillan <firefox1986@gmail.com>
2021-11-21 12:33:35 +0000

    Add first pass at this clone

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore/lite';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2aqAdj4gQ_kiLJ3nQN5g5fYRIaAZuxjo",
+  authDomain: "disneyplus-clone-f4676.firebaseapp.com",
+  projectId: "disneyplus-clone-f4676",
+  storageBucket: "disneyplus-clone-f4676.appspot.com",
+  messagingSenderId: "347532539109",
+  appId: "1:347532539109:web:15fdea35fe70853462d00c"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+export {
+    auth,
+    signInWithPopup,
+    provider
+};
+export default db;

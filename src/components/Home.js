5f0dc2f0d6456d3b2a88b0d0ec86594254812c31
Mike McMillan <firefox1986@gmail.com>
2021-12-05 11:21:44 +0000

    Get docs from data store

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,54 @@ import Originals from "./Originals";
 import Recommends from "./Recommends";
 import Trending from "./Trending";
 import Viewers from "./Viewers";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllDocs } from '../firebase'
+import { setMovies } from '../features/movie/movieSlice'
+import { selectUserName } from '../features/user/userSlice'
 
 const Home = (props) => {
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+
+    useEffect(() => {
+        let recommendDocs = [];
+        let newDisneyDocs = [];
+        let originalDocs = [];
+        let trendingDocs = [];
+        getAllDocs('movies')
+            .then((docs) => {
+                console.log(docs);
+                docs.forEach(doc => {
+                    switch(doc.data().type) {
+                        case 'recommend': 
+                            recommendDocs = [...recommendDocs, {id: doc.id, ...doc.data()}];
+                            break;
+                        case 'new':
+                            newDisneyDocs = [...newDisneyDocs, {id: doc.id, ...doc.data()}];
+                            break;
+                        case 'original':
+                            originalDocs = [...originalDocs, {id: doc.id, ...doc.data()}];
+                            break;
+                        case 'trending':
+                            trendingDocs = [...trendingDocs, {id: doc.id, ...doc.data()}];
+                            break;
+                        default: console.log('wth is a "' + doc.data().type + '"?');
+                    }
+                });
+                dispatch(
+                    setMovies({
+                        recommend: recommendDocs,
+                        newDisney: newDisneyDocs,
+                        original: originalDocs,
+                        trending: trendingDocs
+                    })
+                );
+            }
+        );
+    }, [userName, dispatch]);
+
+
     return (
         <Container>
             <ImageSlider />

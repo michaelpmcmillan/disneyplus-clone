b2336586913aae28844d7c3f79119270108b9cc0
Mike McMillan <firefox1986@gmail.com>
2021-12-05 11:22:25 +0000

    Show content cards from selectRecommend

--- src/components/Recommends.js
+++ src/components/Recommends.js
@@ -1,31 +1,24 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectRecommend } from "../features/movie/movieSlice";
 
 const Recommends = (props) => {
+    const movies = useSelector(selectRecommend);
     return (
         <Container>
             <h4>Recommended for You</h4>
             <Content>
-                <Wrap>
-                    <Link to='/'>
-                        <img src='https://mcdn.wallpapersafari.com/medium/95/16/Yazb65.jpg' alt='' />
-                    </Link>
-                </Wrap>
-                <Wrap>
-                    <Link to='/'>
-                        <img src='https://www.mural-wallpaper.com/wp-content/uploads/2020/11/UN-DIS21.jpg' alt='' />
-                    </Link>
-                </Wrap>
-                <Wrap>
-                    <Link to='/'>
-                        <img src='https://mcdn.wallpapersafari.com/medium/95/16/Yazb65.jpg' alt='' />
-                    </Link>
-                </Wrap>
-                <Wrap>
-                    <Link to='/'>
-                        <img src='https://www.mural-wallpaper.com/wp-content/uploads/2020/11/UN-DIS21.jpg' alt='' />
-                    </Link>
-                </Wrap>
+                {movies &&
+                    movies.map((movie, key) => (
+                        <Wrap key={key}>
+                            {movie.id}
+                            <Link to={'/detail' + movie.id}>
+                                <img src={movie.cardImg} alt={movie.title} />
+                            </Link>
+                        </Wrap>
+                    ))
+                }
             </Content>
         </Container>
     );

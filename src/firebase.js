bb2f470ab3e2f8258ad58846ba72e0ca62b222af
Mike McMillan <firefox1986@gmail.com>
2021-12-05 11:20:15 +0000

    Add getDoc functions

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore/lite';
+import { getFirestore, doc, collection, getDocs } from 'firebase/firestore/lite';
 import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 const firebaseConfig = {
@@ -17,10 +17,20 @@ const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
 
+const getDoc = (docPath) => {
+  return doc(db, docPath);
+};
+
+const getAllDocs = async (collectionName) => {
+  return getDocs(collection(db, collectionName));
+};
+
 export {
     auth,
     signInWithPopup,
     signOut,
-    provider
+    provider,
+    getDoc,
+    getAllDocs
 };
 export default db;

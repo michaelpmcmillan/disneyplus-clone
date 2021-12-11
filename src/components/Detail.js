7d941f7f896d51fd365704f3d92cf470ff90a3fa
Mike McMillan <firefox1986@gmail.com>
2021-12-11 09:36:27 +0000

    Add group-watch button

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -33,6 +33,12 @@ const Detail = (props) => {
                         <span/>
                         <span/>
                     </AddList>
+
+                    <GroupWatch>
+                        <div>
+                        <img src='/images/group-icon.png' alt='' />
+                        </div>
+                    </GroupWatch>
                 </Controls>
             </ContentMeta>
         </Container>
@@ -165,6 +171,28 @@ const AddList = styled.div`
             height: 16px;
             transform: translate(-8px) rotate(0deg);
             width: 2px;
+        }
+    }
+`;
+
+const GroupWatch = styled.div`
+    height: 44px;
+    width: 44px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    background: white;
+
+    div {
+        height: 40px;
+        width: 40px;
+        background: rgb(0, 0, 0);
+        border-radius: 50%;
+
+        img {
+            width: 100%;
         }
     }
 `;

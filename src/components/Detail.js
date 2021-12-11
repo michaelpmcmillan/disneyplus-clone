a1e4363453171502bf9c1a6b5d98d788bcf483f2
Mike McMillan <firefox1986@gmail.com>
2021-12-11 08:54:57 +0000

    Add initial styling

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 
 const Detail = (props) => {
-    return (
-        <div>
-            <div>Detail</div>
-        </div>
-    );
+    return <Container>Details</Container>;
 };
+
+const Container = styled.div`
+    position: relative;
+    min-height: calc(100vh-250px);
+    overflow-x: hidden;
+    display: block;
+    top: 72px;
+    padding: 0 calc(3.5vw + 5px);
+`;
 
 export default Detail;

0bcacee6845be24cb61fb5ec34b3e5c9e8472d23
Mike McMillan <firefox1986@gmail.com>
2021-12-11 09:03:41 +0000

    Add hardcoded image

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 
 const Detail = (props) => {
-    return <Container>Details</Container>;
+    return (
+        <Container>
+            <Background>
+                <img
+                    src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4E9E81584305009D6385F6178D4B6930E97CD6EC4A3B53C818400DEF778FFA9A/scale?width=1440&aspectRatio=1.78&format=jpeg'
+                    alt=''
+                />
+            </Background>
+        </Container>
+    );
 };
 
 const Container = styled.div`
@@ -11,6 +20,25 @@ const Container = styled.div`
     display: block;
     top: 72px;
     padding: 0 calc(3.5vw + 5px);
+`;
+
+const Background = styled.div`
+    left: 8px;
+    opacity: 0.8;
+    position: fixed;
+    right: 0px;
+    top: 0px;
+    z-index: -1;
+
+    img {
+        width: 100vw;
+        height: 100vh;
+
+        @media (max-width: 768px) {
+            width: initial;
+        }
+    }
+
 `;
 
 export default Detail;
